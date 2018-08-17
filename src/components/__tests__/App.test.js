faeebcbe25e7e747bcc8cbcbe5b2f61bbbcc2c90
ntrukhanovets <n.trukhanovets@gmail.com>
2018-08-17 14:43:29 +0300

    absolute path

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.react';
+import App from 'components/App.react';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
